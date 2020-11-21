5eb0e1973f3f19e27b6a4de9abd85a8c1bc5656f
beerwithstraw <agarwalp@tcd.ie>
2020-11-21 17:47:22 +0000

    alan-ai button import

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+import alanBtn from '@alan-ai/alan-sdk-web';
+const alanKey = '9ec459bed89444fe2529ec941682e2892e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+        })
+    },[])
+
+    return(
+        <div>
+        <h1>Alan AI News Application</h1>
+        </div>
+    )
+}
+
+export default App;
