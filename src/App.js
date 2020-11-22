166c498b672c9b4286f1be13c6527f1b4967289c
beerwithstraw <agarwalp@tcd.ie>
2020-11-22 02:39:41 +0000

    Static Display Cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import NewsCards from './components/NewsCards/NewsCards';
-
 import alanBtn from '@alan-ai/alan-sdk-web';
+import useStyles from './styles.js'
+
 const alanKey = '9ec459bed89444fe2529ec941682e2892e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles(); 
 
     useEffect(() => {
         alanBtn({
@@ -23,7 +25,10 @@ const App = () => {
 
     return(
         <div>
-        <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="Alan Logo"/>
+
+            </div>
         <NewsCards articles = {newsArticles} />
         </div>
     )
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
-
+import useStyles from './styles.js'
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i}) => {
+    
+    const classes = useStyles();
+    
         return (
-            <Card>
-                
-                <CardActionArea>
-                    <CardMedia image={ urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}/>
+            
+            <Card className={classes.card}>               
+                <CardActionArea href={url} target="_blank">
+                    <CardMedia className={classes.media}image={ urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}/>
 
-                    <div>
-                        <Typography variant="body2" color="textSecondary" component="h2"></Typography>
-                        <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+                    <div className={classes.details}> 
+                        <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                        <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                     </div>
 
-                    <Typography gutterBottom variant="h5"></Typography>
+                    <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
 
                     <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                     </CardContent>
                 </CardActionArea>
 
-                <CardActions>
-                    <Button size="small" color="primary"></Button>
-                    <Typography variant="h5" color="textSecondary"></Typography>
-
+                <CardActions className={classes.cardActions}>
+                    <Button size="small" color="primary">Read Full Story</Button>
+                    <Typography variant="h5" color="textSecondary">{i+1}</Typography>
                 </CardActions>
 
             </Card>
-
         );
     }
 
