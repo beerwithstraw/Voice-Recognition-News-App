6c21988b9490399dd3dce52e6fb8e98275f24b2d
beerwithstraw <agarwalp@tcd.ie>
2020-11-22 01:18:53 +0000

    News Card Structure

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
-const NewsCard = () => {
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i}) => {
         return (
-            <div>
-                News Card
-            </div>
+            <Card>
+                
+                <CardActionArea>
+                    <CardMedia image={ urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}/>
+
+                    <div>
+                        <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+                        <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+                    </div>
+
+                    <Typography gutterBottom variant="h5"></Typography>
+
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                    </CardContent>
+                </CardActionArea>
+
+                <CardActions>
+                    <Button size="small" color="primary"></Button>
+                    <Typography variant="h5" color="textSecondary"></Typography>
+
+                </CardActions>
+
+            </Card>
+
         );
     }
 
