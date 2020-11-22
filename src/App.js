38efcc26c0e711ef62187314c67caf62d72e7938
beerwithstraw <agarwalp@tcd.ie>
2020-11-22 00:29:05 +0000

    working api console log

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import alanBtn from '@alan-ai/alan-sdk-web';
 const alanKey = '9ec459bed89444fe2529ec941682e2892e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand:({ command }) => {
-                if (command === 'testCommand') {
-                    alert('Alert command executed');
+            onCommand:({ command, articles}) => {
+                if (command === 'newHeadlines') {
+                    console.log(articles);
                 }
             }
         })
