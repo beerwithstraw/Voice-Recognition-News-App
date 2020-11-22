94aa87ea965c986cfd467247bf4dcd97d31a2823
beerwithstraw <agarwalp@tcd.ie>
2020-11-22 00:54:03 +0000

    News Cards Display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
+import NewsCards from './components/NewsCards/NewsCards';
+
 import alanBtn from '@alan-ai/alan-sdk-web';
 const alanKey = '9ec459bed89444fe2529ec941682e2892e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
@@ -11,6 +14,7 @@ const App = () => {
             key: alanKey,
             onCommand:({ command, articles}) => {
                 if (command === 'newHeadlines') {
+                    setNewsArticles(articles);
                     console.log(articles);
                 }
             }
@@ -20,6 +24,7 @@ const App = () => {
     return(
         <div>
         <h1>Alan AI News Application</h1>
+        <NewsCards articles = {newsArticles} />
         </div>
     )
 }
